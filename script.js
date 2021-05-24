157a42b7638a4813f597d2a1de6d177ed62a1989
Viviele Oliveira <viviele.oliveira@gmail.com>
2021-05-24 13:45:09 -0300

    criar cobrinhamovimentos

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 // função criar background
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,6 +22,27 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+    let newHead = {
+        x = snakeX,
+        y = snakeY
+    }
+    snake.unshift(newHead);
+
+}
+let jogo = setInterval(IniciarJogo, 100);
+
+
 
