3347919fa5129794ff79e73563b0bafd6ff71a97
Viviele Oliveira <viviele.oliveira@gmail.com>
2021-05-24 12:29:06 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 // função criar background
 function criarBG() {
@@ -8,6 +13,13 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG() {
-    
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
+
+criarBG();
+criarCobrinha();
+
