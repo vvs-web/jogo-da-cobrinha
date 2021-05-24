09ea26b3dc1958c060747ff70f988ad93ac4e0b7
Viviele Oliveira <viviele.oliveira@gmail.com>
2021-05-24 12:12:57 -0300

    criar background

---
+++ script.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+
+// função criar background
+function criarBG() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+criarBG() {
+    
+}
